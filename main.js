7601e8f8b6774b0bdc95770cc1c49c697d857ce0
Mac <94739622+NewNougat@users.noreply.github.com>
2022-08-17 13:16:22 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+//examine the document object
+
+//console.dir(document);
+//console.log(document.domain);
+//console.log(document.URL);
+//console.log(document.title);
+//document.title = 123;
+console.log(document.doctype);
+console.log(document.body);
+console.log(document.all[10]);
+//document.all[10].textContent = 'fuckyou';
+console.log(document.forms);
+
+
